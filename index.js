a7f3905be4a1bca4e420564fd68bd92331c2d6a6
Denis <103854079+tkachukds@users.noreply.github.com>
2023-05-03 05:52:47 +0300

    add stndrd file

---
+++ index.js
@@ -0,0 +1,13 @@
+function load_header() {
+    document.getElementById("header").innerHTML='<object type="text/html" data="header.html" ></object>';
+}
+
+function load_box() {
+    document.getElementById("box").innerHTML='<object type="text/html" data="create plane.html" ></object>';
+}
+
+
+window.onload = function() {
+load_header();
+load_box();
+}
