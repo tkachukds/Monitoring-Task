9fba6320bfbea163300f371bb4709b88f5eb9ef5
Denis <103854079+tkachukds@users.noreply.github.com>
2023-05-03 10:44:17 +0300

    update style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function load_header() {
-    document.getElementById("header").innerHTML='<object type="text/html" data="header.html" style="width: 100%";></object>';
+    document.getElementById("header").innerHTML='<object type="text/html" data="header.html" style="width: 100%;height: 100%;"></object>';
 }
 
 function load_box() {
-    document.getElementById("boxPLANECREATER").innerHTML='<object type="text/html" data="create plane.html" style="width: 100%";></object>';
+    document.getElementById("boxPLANECREATER").innerHTML='<object type="text/html" data="create plane.html" style="width: 100%;height: 100%;"></object>';
 }
 
 
